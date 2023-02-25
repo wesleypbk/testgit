aead59f9b11213e8a75247228aae2c72bb50db3e
wesleypbk <110872032+wesleypbk@users.noreply.github.com>
2023-02-24 21:32:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({ message: "Olá mundo estou funcionando"});
 })
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({ message: "oi eu sou a rota 'olá'"});
+})
+
 app.listen(3001, () =>{
     console.log('Api rodando na porta 3001');
 })
