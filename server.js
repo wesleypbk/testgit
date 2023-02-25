642805859a45d8860cf0d9197fde6a358a97eacb
Wesley <wesleygon1612@gmail.com>
2023-02-24 21:22:51 -0300

    Criado o servidor api com express

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: "Olá mundo estou funcionando"});
+})
+
+app.listen(3001, () =>{
+    console.log('Api rodando na porta 3001');
+})
 
